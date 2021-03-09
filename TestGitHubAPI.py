7406f46cf072acf84f4d38c942225cc7bc241b13
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2021-03-09 17:44:34 -0500

    final changes

--- TestGitHubAPI.py
+++ TestGitHubAPI.py
@@ -13,4 +13,3 @@ if __name__ == '__main__':
     unittest.main()
 
 
-# ['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 9', 'Repo: threads-of-life Number of commits: 1', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 5']
