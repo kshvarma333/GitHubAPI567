3c05f40058f2e3249b8c55df2e30b4a673afaf7a
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2021-03-09 14:59:51 -0500

    Update TestGitHubAPI.py

--- TestGitHubAPI.py
+++ TestGitHubAPI.py
@@ -8,12 +8,9 @@ class TestGitHubAPI(unittest.TestCase):
         lis = git_hub_api("richkempinski")
         self.assertEqual(lis[0],'Repo: csp Number of commits: 2')
 
-def main():
-    unittest.main(exit=False, verbosity=2)
-
-
 if __name__ == '__main__':
-    main()
+    print('Running unit tests')
+    unittest.main()
 
 
 # ['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 9', 'Repo: threads-of-life Number of commits: 1', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 5']
