8a4c44e63759d33338b91522552788d3d6f72346
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2021-03-09 14:52:03 -0500

    Final commit
    added test file

--- GitHubAPI.py
+++ GitHubAPI.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-def git_hub_api(name_of_repo:str) -> str:
+def git_hub_api(name_of_repo:str) -> list:
+    answer:list = []
     url:str = f"https://api.github.com/users/{name_of_repo}/repos"
     list_of_repos: list = requests.get(url).json()
     for item in list_of_repos:
@@ -9,7 +10,7 @@ def git_hub_api(name_of_repo:str) -> str:
         url2:str = f"https://api.github.com/repos/{name_of_repo}/{item['name']}/commits"
         list_of_commits:list = requests.get(url2).json()
         count = len(list_of_commits)
-        print(f'Repo: {repo_name} Number of commits: {count}') 
-        
+        temp:str = f'Repo: {repo_name} Number of commits: {count}'
+        answer.append(temp)
+    return answer
 
-git_hub_api("kshvarma333")
---
+++ TestGitHubAPI.py
@@ -0,0 +1,19 @@
+import unittest
+
+from GitHubAPI import git_hub_api
+
+class TestGitHubAPI(unittest.TestCase):
+    def testgithubapi(self):
+        """ Testing GitHubAPI"""
+        lis = git_hub_api("richkempinski")
+        self.assertEqual(lis[0],'Repo: csp Number of commits: 2')
+
+def main():
+    unittest.main(exit=False, verbosity=2)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# ['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 9', 'Repo: threads-of-life Number of commits: 1', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 5']
