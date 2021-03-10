2583cfd2f4c8a401506f2a2cebbfe0989da3f00d
SRI HARSHA VARMA KONDA <57238008+kshvarma333@users.noreply.github.com>
2021-03-09 23:20:36 -0500

    Update TestGitHubAPI.py

--- TestGitHubAPI.py
+++ TestGitHubAPI.py
@@ -2,12 +2,12 @@ import unittest
 
 from GitHubAPI import git_hub_api
 
-from typing import List
+from typing import List,Any
 
 class TestGitHubAPI(unittest.TestCase):
     def testgithubapi(self):
         """ Testing GitHubAPI"""
-        lis: List = git_hub_api("richkempinski")
+        lis: List[Any] = git_hub_api("richkempinski")
         self.assertEqual(lis[0],'Repo: csp Number of commits: 2')
 
 if __name__ == '__main__':
